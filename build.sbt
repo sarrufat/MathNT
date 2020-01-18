7c90e42d222af99ab923df9db8d2cf10b2f87031
sarrufat <arrufat.sergi@gmail.com>
2020-01-18 10:32:41 +0100

    First Fibonacci

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "MathNT"
 version := "0.1"
 
 scalaVersion := "2.13.1"
+
+libraryDependencies ++= Seq(
+  "nl.grons" %% "metrics4-scala" % "4.1.1"
+)
---
+++ src/main/scala/org/math/theory/number/fibonacci/Fibonacci.scala
@@ -0,0 +1,46 @@
+package org.math.theory.number.fibonacci
+
+import java.util.concurrent.TimeUnit
+
+import com.codahale.metrics.ConsoleReporter
+import nl.grons.metrics4.scala.DefaultInstrumented
+
+object Fibonacci extends App with DefaultInstrumented {
+  val reporter = ConsoleReporter.forRegistry(metricRegistry)
+  val rb = reporter.build()
+  rb.start(1, TimeUnit.SECONDS)
+
+
+  // Define a timer metric
+  private[this] val fibonacciTimer = metrics.timer("fibonacci")
+  private[this] val fibonacciTimer2 = metrics.timer("fiboOpt")
+
+  // Not optimized fibonacci: takes a lot of time
+  def fibonacci(n: Long): Long = fibonacciTimer.time {
+    if (n > 1)
+      fibonacci(n - 1) + fibonacci(n - 2)
+    else
+      n
+  }
+
+  def optFibonacci(n: Long, a: Long, b: Long): Long = fibonacciTimer2.time {
+    if (n == 0) a
+    else optFibonacci(n - 1, b, a + b)
+  }
+
+  /* assert(fibonacci(2) == 1)
+   assert(fibonacci(3) == 2)
+   assert((fibonacci(7) == 13)) */
+  var retsult = 0L
+  //fibonacciTimer.time {
+
+  // }
+  val tetsValue = 35
+  retsult = fibonacci(tetsValue)
+  retsult = optFibonacci(tetsValue, 0, 1)
+  println(s"fibonacci($tetsValue) = $retsult")
+  println(s"optFibonacci($tetsValue) = $retsult")
+
+  rb.report()
+
+}
