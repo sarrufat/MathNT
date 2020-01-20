df60403c7f85df6a60305c4d5e9cb1b66c91f2e1
sarrufat <arrufat.sergi@gmail.com>
2020-01-20 18:39:42 +0100

    Divisibility:  factorize

--- src/main/scala/org/math/theory/number/Divisibility.scala
+++ src/main/scala/org/math/theory/number/Divisibility.scala
@@ -1,7 +1,7 @@
 package org.math.theory.number
 
 object Divisibility {
- private  def gcdEuclidean(a: BigInt, b: BigInt): BigInt = {
+  private def gcdEuclidean(a: BigInt, b: BigInt): BigInt = {
     @scala.annotation.tailrec
     def div(a: BigInt, b: BigInt): BigInt = {
       if (b == 0) a
@@ -25,5 +25,17 @@ object Divisibility {
       val z = gcdEuclidean(a, b)
       args.dropRight(2).foldRight(z) { (a, b) => gcdEuclidean(a, b) }
     }
+  }
+
+  def factorize(n: BigInt): Seq[BigInt] = {
+    def fact_(n: BigInt, acc: BigInt): Seq[BigInt] =
+      if (acc * acc > n) Seq(n)
+      else
+        (n % acc).toInt match {
+          case 0 => acc +: fact_(n / acc, acc)
+          case _ => fact_(n, acc + 1)
+        }
+
+    fact_(n, 2)
   }
 }
--- src/test/scala/factest/DivTest.scala
+++ src/test/scala/factest/DivTest.scala
@@ -20,7 +20,16 @@ class DivTest extends AnyFreeSpec with Matchers {
       Divisibility.gcdEuclidean(140, 350) shouldBe 70
       Divisibility.gcdEuclidean(105, 140, 350) shouldBe 35
       // Mutually relatively prime
-      Divisibility.gcdEuclidean(15,21,35) shouldBe 1
+      Divisibility.gcdEuclidean(15, 21, 35) shouldBe 1
+    }
+  }
+
+  "Factors" - {
+    "Factorize" in {
+      val testn = 1111*3*18*104729
+      val result = Divisibility.factorize(testn).map(_.toInt)
+      result shouldBe Seq(2, 5, 61, 3259253)
+      Divisibility.gcdEuclidean(testn, 3259253).toInt shouldBe 3259253
     }
   }
 }
