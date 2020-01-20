9825f98afce9faaf0f5088acf808629f01d4577f
sarrufat <arrufat.sergi@gmail.com>
2020-01-20 16:52:43 +0100

    Divisibility:  GCD

--- src/test/scala/factest/FibonacciTest.scala
+++ src/test/scala/factest/FibonacciTest.scala
@@ -1,5 +1,6 @@
 package factest
 
+import org.math.theory.number.Divisibility
 import org.math.theory.number.fibonacci.Fibonacci
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
@@ -10,6 +11,12 @@ class FibonacciTest extends AnyFreeSpec with Matchers {
     "binomialFibonacci compare results with optFibonacci" in {
       val N = 2148
       Fibonacci.binomialFibonacci(N) shouldBe Fibonacci.optFibonacci(N)
+    }
+
+    "relative primes of two consecutive" in {
+      val N = 1512
+      for (n <- 100 to N)
+        Divisibility.gcdEuclidean(Fibonacci.optFibonacci(n + 1), Fibonacci.optFibonacci(n)) shouldBe 1
     }
   }
 }
